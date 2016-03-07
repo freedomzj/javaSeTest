3cd97470afb28693f8708e5c875e7a8d2407f7d2
1063304800@qq.com <1063304800@qq.com>
2016-03-07 09:38:59 +0800

    aa

--- src/com/io/JavaIo.java
+++ src/com/io/JavaIo.java
@@ -1,41 +1,76 @@
 package com.io;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
-import org.junit.Test;
+import javax.xml.bind.DatatypeConverter;
 
 public class JavaIo {
 
-	@Test
-	public void test() {
-		writeFile();
+	public static void main(String[] args) {
+//		new JavaIo().new myTestIO().run();
+		for(String fileName: args){
+			new JavaIo().new DigestThread(fileName).start();
+		}
+		
 	}
 
-	public void writeFile() {
-		File file = new File("logo.jpg");
-		try {
-			
-			InputStream fis = new FileInputStream(file);
-			FileOutputStream fos = new FileOutputStream("D:\\logo.jpg");
-			byte[] b = new byte[1024];
-			int temp = 0;
-			while ((temp = fis.read(b)) != -1) {
-				fos.write(b, 0, temp);
+	private class myTestIO implements Runnable {
+
+		@Override
+		public void run() {
+			File file = new File("logo.jpg");
+			try (InputStream fis = new FileInputStream(file);
+					FileOutputStream fos = new FileOutputStream("D:\\logo.jpg");) {
+				byte[] b = new byte[1024];
+				int temp = 0;
+				while ((temp = fis.read(b)) != -1) {
+					fos.write(b, 0, temp);
+				}
+				fos.flush();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-			fis.close();
-			fos.flush();
-			fos.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+			System.out.println(file.length());
 		}
 
-		System.out.println(file.length());
+	}
+	
+	
+	private class DigestThread extends Thread{
+		private String fileName;
+		public DigestThread(String fileName){
+			this.fileName=fileName;
+		}
+		public void run() {
+			try{
+				FileInputStream in =new FileInputStream(fileName);
+				MessageDigest sha=MessageDigest.getInstance("SHA-256");
+				DigestInputStream din=new DigestInputStream(in,sha);
+				while(din.read()!=-1){
+					din.close();
+					byte[] digest=sha.digest();
+					StringBuilder result=new StringBuilder(fileName);
+					result.append(":");
+					result.append(DatatypeConverter.printHexBinary(digest));
+					System.out.println(result);
+				}
+			}catch(IOException iex){
+				System.out.println(iex.getMessage());
+			} catch (NoSuchAlgorithmException e) {
+				e.printStackTrace();
+			}
+			
+		}
 	}
 
 }
